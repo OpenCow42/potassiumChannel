958feef3b26a306cabf25af4d2b7d6613fa9767a
OpenCow <opencow@ik.me>
2026-05-07 12:22:55 +0200

    Add kChat public team channels route

--- Sources/potassiumChannel/Infomaniak/KChat/KChatModels.swift
+++ Sources/potassiumChannel/Infomaniak/KChat/KChatModels.swift
@@ -179,6 +179,21 @@ public struct KChatTeamMembersOptions: Equatable, Sendable {
     }
 }
 
+/// Query options accepted by the Mattermost-compatible kChat public team channels endpoint.
+public struct KChatPublicChannelsForTeamOptions: Equatable, Sendable {
+    /// The page to select.
+    public let page: Int?
+
+    /// The number of public channels per page.
+    public let perPage: Int?
+
+    /// Creates kChat public team channels query options.
+    public init(page: Int? = nil, perPage: Int? = nil) {
+        self.page = page
+        self.perPage = perPage
+    }
+}
+
 /// A Mattermost-compatible kChat team statistics response.
 public struct KChatTeamStats: Codable, Equatable, Sendable {
     /// Team identifier these statistics belong to.
